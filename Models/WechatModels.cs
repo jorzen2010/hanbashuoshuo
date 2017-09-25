424cadceb5a61064ac0bb6a0cad9144c043eac3c
jorzen <jorzen2010@163.com>
2017-09-25 16:09:52 +0800

    初始化

---
+++ Models/WechatModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hanbashuoshuo.Models
+{
+    public class WechatConfig
+    {
+        public string wechatID { get; set; }
+        public string appID { get; set; }
+        public string appsecret { get; set; }
+        public string appURL { get; set; }
+        public string appToken { get; set; }
+
+        public string JsToken { get; set; }
+        public string JsDomain { get; set; }
+        public string JsReturnDomain { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessTokenExpiredTime { get; set; }
+
+    }
+}
